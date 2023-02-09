e79700526d2c34d3c30dc010628acd4cab8886a0
Eduardo Sampaio <edupcsampaio@gmail.com>
2023-02-09 16:41:09 -0300

    create base project

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Tweet } from './components/Tweet';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <div>
+      <Tweet />
+    </div>
+  </React.StrictMode>
+);
