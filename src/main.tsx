b8d72ebd86dcc778e8f0369f4acfdad0c0c514b6
Eduardo Sampaio <edupcsampaio@gmail.com>
2023-02-10 15:13:36 -0300

    create components

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,38 @@ import ReactDOM from 'react-dom/client';
 
 import './global.css';
 
+import { Sidebar } from './components/Sidebar';
+import { Header } from './components/Header';
+import { Tweet } from './components/Tweet';
+import { Separator } from './components/Separator';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <div className="layout">
+      <Sidebar />
+
+      <div className="content">
+        <main className="timeline">
+          <Header title="Home" />
+
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img
+                src="https://github.com/edusmpaio.png"
+                alt="Eduardo Sampaio"
+              />
+              <textarea id="tweet" placeholder="What's happening" />
+            </label>
+
+            <button type="submit">Tweet</button>
+          </form>
+
+          <Separator />
+
+          <Tweet />
+          <Tweet />
+        </main>
+      </div>
+    </div>
+  </React.StrictMode>
 );
